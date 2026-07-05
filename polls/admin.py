309cb4d10d0a21de6663caaf45f6063682bab2fa
Samarth Khurana <khuranasam14@gmail.com>
2026-07-05 19:22:56 +0000

    Register models in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
-from .models import Question
+from .models import (
+    Question,
+    Choice,
+    Project,
+    Employee,
+    Comment,
+    IdempotencyKey,
+)
 
 admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Project)
+admin.site.register(Employee)
+admin.site.register(Comment)
+admin.site.register(IdempotencyKey)
